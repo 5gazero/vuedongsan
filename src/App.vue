ce3cc430b98d9f37fe0bfb24b9d317214fce5cbd
오가영 <kayo9410@gmail.com>
2022-06-09 17:34:29 +0900

    [mod] modal

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
+  <div class="black-bg" v-if="isModalOpen">
+    // true일때만,
+    <div class="white-bg">
+      <h4>
+        상세페이지 <span class="btn" @click="isModalOpen = false">X</span>
+      </h4>
+      <p>설명</p>
+    </div>
+  </div>
   <div class="menu">
     <a v-for="(menu, idx) in menus" :key="idx">{{ menu }}</a>
   </div>
-  <div>
-    <h4 v-for="(product, idx) in products" :key="idx">{{ product }}</h4>
-    <p>{{ price }}만원</p>
-    <button @click="increase">허위매물신고</button>
-    <span>신고수: {{ count }}</span>
+  <div v-for="(product, idx) in products" :key="idx">
+    <img src="./assets/room0.jpg" class="img" />
+    <h4 class="btn" @click="isModalOpen = true">{{ product }}</h4>
+    <p>{{ price[idx] }}만원</p>
+    <button @click="count[idx]++">허위매물신고</button>
+    <span>신고수: {{ count[idx] }}</span>
   </div>
 </template>
 
@@ -17,10 +27,16 @@ export default {
   name: "App",
   data() {
     return {
-      count: 0,
+      isModalOpen: false,
+      count: [0, 0, 0],
       menus: ["Home", "Products", "About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
-      price: 60,
+      price: [60, 55, 65],
+      images: [
+        "./assets/room0.jpg",
+        "./assets/room1.jpg",
+        "./assets/room2.jpg",
+      ],
     };
   },
   methods: {
@@ -40,6 +56,13 @@ export default {
   color: #2c3e50;
 }
 
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+
 .menu {
   background: yellowgreen;
   padding: 15px;
@@ -49,5 +72,29 @@ export default {
   color: white;
   padding: 10px;
   font-weight: 600;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 10vh 10vw;
+}
+.white-bg {
+  width: 100%;
+  height: 80vh;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.img {
+  margin-top: 20px;
+}
+.btn {
+  cursor: pointer;
+}
+.btn:hover {
+  color: yellowgreen;
 }
 </style>
